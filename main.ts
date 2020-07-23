44f1d8043a4c2644dccae20f8fc960aa7f8b54e4
nbirtegue <68041767+nbirtegue@users.noreply.github.com>
2020-07-23 14:51:29 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ basic.forever(function () {
     y1 = -1
     y = 0
     x = randint(0, 4)
-    basic.pause(300)
+    basic.pause(50)
     while (y2 != 5) {
         led.plot(x, y2)
         led.plot(x, y1)
@@ -15,7 +15,7 @@ basic.forever(function () {
         led.plotBrightness(x, y, 255)
         led.plotBrightness(x, y1, 185)
         led.plotBrightness(x, y2, 115)
-        basic.pause(300)
+        basic.pause(50)
         led.unplot(x, y2)
         led.unplot(x, y1)
         led.unplot(x, y)
